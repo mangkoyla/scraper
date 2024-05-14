fa7bb4364865de5d9f23f91c29c69f47f8b79d7c
mangkoyla <160372172+mangkoyla@users.noreply.github.com>
2024-05-15 00:24:21 +0700

    Delete vidio.sh

--- vidio.sh
+++
@@ -1,8 +0,0 @@
-linksvidio=(
-    "https://mangkoyla.vercel.app/api/raw?vpn=vmess,vless,trojan&mode=cdn&cdn=quiz.int.vidio.com&nw=ws"
-    )
-
-for linksvidio in "${linksvidio[@]}"; do
-    curl -s "$linksvidio" >> nodesvidio
-    echo "Downloaded $linksvidio"
-done
